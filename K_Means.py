fc8032bcebd3c391f6eb5293c35fb5a3c9d296b4
abdo harp <abdoharp234@gmail.com>
2023-09-29 10:06:58 +0300

    K_means fille

--- K_Means.py
+++ K_Means.py
@@ -21,5 +21,40 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.svm import SVC
 from sklearn.datasets import load_breast_cancer
+from sklearn.cluster import KMeans
+
+customers = pd.read_csv("D:/data/mallcustomers.csv")
+print(customers.head())
+print(customers.info())
+print(customers.describe().T)
+
+customers.drop(columns="CustomerID", inplace=True)
+# print(customers.head())
 
 
+# sns.boxplot(data=customers , x="Gender", y="Income")
+# sns.boxplot(data=customers , x="Gender", y="Age")
+# sns.boxplot(data=customers , x="Gender", y="SpendingScore")
+# sns.scatterplot(data=customers , x="Age", y="Income",s=100)
+# sns.scatterplot(data=customers , x="Income", y="SpendingScore",s=100 , hue="Gender")
+
+sc = StandardScaler()
+customers_scaled = sc.fit_transform(customers[["Income", "SpendingScore"]])
+customers_scaled = pd.DataFrame(customers_scaled, columns=["Income", "SpendingScore"])
+# print(customers.head())
+# print(customers.describe().T)
+
+km = KMeans(n_clusters=3, n_init=10, random_state=1234)
+km.fit(customers_scaled)
+print(km.labels_)
+print(km.inertia_)
+print(pd.Series(km.labels_).value_counts())
+print(km.cluster_centers_)
+cluster_cent = pd.DataFrame(km.cluster_centers_, columns=["Income", "SpendingScore"])
+ax = sns.scatterplot(data=customers_scaled, x="Income", y="SpendingScore", hue=km.labels_,marker="x", alpha=0.8, s=120, legend=False)
+ax= sns.scatterplot(data=cluster_cent, x="Income", y="SpendingScore", hue=cluster_cent.index, palette="colorblind", alpha=0.8, s=250, marker="v", legend=False)
+
+for i in range(len(cluster_cent)):
+    plt.text(x=cluster_cent.Income[i], y= cluster_cent.SpendingScore[i],s=i)
+
+plt.show()
